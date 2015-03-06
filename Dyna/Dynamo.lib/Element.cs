983cb83597d36b24c040177444230ff6994abe6f
maikeru.r <miker_asky@hotmail.com>
2015-03-06 20:13:07 +0800

    DynaCallbackLib

---
+++ Dyna/Dynamo.lib/Element.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.lib
+{
+    public class Element
+    {
+        ElementType elType;
+
+        private static readonly object syncLock = new object();
+
+        public Element()
+        {
+            lock (syncLock)
+            {
+                Random rnd = new Random();
+                elType = (ElementType)rnd.Next(3);
+            }
+        }
+
+        public ElementType PickElement()
+        {
+            return elType;
+        }
+    }
+
+    public enum ElementType
+    {
+        Fire = 0,
+        Water,
+        Wind
+    }
+}
---
+++ Dyna/Lib.TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string application = Assembly.GetExecutingAssembly().GetName().Name;
+            string solutionPath = path.Split(new string[] { application }, StringSplitOptions.RemoveEmptyEntries)[0];
+            string dynamoAssm = string.Concat(solutionPath,"Dynamo.lib\\bin\\Debug\\Dynamo.lib.dll");
+            Assembly assm = Assembly.LoadFile(dynamoAssm);
+
+            object e = assm.CreateInstance("Dynamo.lib.Element");
+            Type dynamoLib = e.GetType();
+            ConstructorInfo dynamoLibConstructor = dynamoLib.GetConstructor(Type.EmptyTypes);
+
+            object dynamoClsObj = dynamoLibConstructor.Invoke(new object[] { });
+
+
+            object dynamoVal = dynamoLib.GetMethod("PickElement").Invoke(dynamoClsObj, null);
+
+            Console.Write(AppDomain.CurrentDomain.BaseDirectory);
+            //Assembly assm = Assembly.LoadFile("");
+        }
+    }
+}
